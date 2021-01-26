f7dab076a813ae28e0fd6d6211cbcc186d6e7273
Braden <bmw02002turbo@gmail.com>
2021-01-26 09:20:17 -0800

    Added Sign Out Button

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -4,6 +4,10 @@
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span><span>App</span>
       </v-toolbar-title>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon>mdi-exit-to-app</v-icon>
+      </v-btn>
     </v-toolbar>
   </nav>
 </template>
