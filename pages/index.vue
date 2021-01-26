69c407ecd0abc12952ddf8f06a54d7884542bad6
Braden <bmw02002turbo@gmail.com>
2021-01-26 11:42:26 -0800

    Added project title and person to dashboard grid

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,12 @@
   <div class="dashboard">
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-card>
-        <v-row> <v-col cols="12" md="6"></v-col></v-row
-      ></v-card>
+      <v-card class="pa-3">
+        <v-row>
+          <v-col cols="12" md="6">Project Title</v-col>
+          <v-col cols="6" md="3">Person</v-col>
+        </v-row></v-card
+      >
     </v-container>
   </div>
 </template>
