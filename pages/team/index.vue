6f8599a3c7bdc7130c5bd1a03cafccdced552c94
Braden <bmw02002turbo@gmail.com>
2021-01-26 14:12:07 -0800

    Added image cards

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -3,7 +3,28 @@
     <h1 class="subtitle-1 grey--text">Team</h1>
     <v-container class="my-5">
       <v-row>
-        <v-col v-for="person in persons" xs="12" sm="6" md="4" lg="3"></v-col>
+        <v-col
+          v-for="person in team"
+          :key="person.name"
+          xs="12"
+          sm="6"
+          md="4"
+          lg="3"
+        >
+          <v-card class="text-xs-center ma-3">
+            <v-responsive class="pt-4">Image goes here</v-responsive>
+            <v-card-text>
+              <div class="subtitle-1">{{ person.name }}</div>
+              <div class="grey--text">{{ person.role }}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn color="light--grey">
+                <v-icon small left>mdi-message</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
       </v-row>
     </v-container>
   </div>
