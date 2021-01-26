8b199d48140d04715e3042b5f101f8f829fb5044
Braden <bmw02002turbo@gmail.com>
2021-01-26 11:40:23 -0800

    Replace tests with v-card

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,9 @@
   <div class="dashboard">
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-row>
-        <v-col cols="12" md="6" m-6
-          ><v-btn outline block class="primary">1</v-btn>
-        </v-col>
-      </v-row>
+      <v-card>
+        <v-row> <v-col cols="12" md="6"></v-col></v-row
+      ></v-card>
     </v-container>
   </div>
 </template>
