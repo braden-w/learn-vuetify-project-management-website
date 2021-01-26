6b49f5aaf1cd032b2472b6fc0120e1e62a39063a
Braden <bmw02002turbo@gmail.com>
2021-01-26 10:30:18 -0800

    v-for that loops through links

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -14,12 +14,19 @@
     </v-toolbar>
     <v-navigation-drawer v-model="drawer" app class="primary">
       <v-list>
-        <v-list-item>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+        >
           <v-list-item-action>
-            <v-icon class="white--text">mdi-view-dashboard</v-icon>
+            <v-icon class="white--text">{{ link.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title class="white--text">Dashboard</v-list-item-title>
+            <v-list-item-title class="white--text">{{
+              link.text
+            }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
