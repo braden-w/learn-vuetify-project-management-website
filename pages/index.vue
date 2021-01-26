0357620c4927135e38c8e7978dc7dc4bdb173387
Braden <bmw02002turbo@gmail.com>
2021-01-26 08:26:29 -0800

    Added flat pink button

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <h1>Homepage</h1>
     <v-btn class="pink white--text">Click me</v-btn>
     <v-btn depressed color="pink">Click me</v-btn>
+    <v-btn flat color="pink">Click me</v-btn>
   </div>
 </template>
 
