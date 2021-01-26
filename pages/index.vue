f7f9ab7b71307a8df819d437c08ceb16d5fa6c2f
Braden <bmw02002turbo@gmail.com>
2021-01-26 08:26:36 -0800

    Added depressed button with white text

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <v-btn class="pink white--text">Click me</v-btn>
     <v-btn depressed color="pink">Click me</v-btn>
     <v-btn flat color="pink">Click me</v-btn>
+    <v-btn depressed class="pink white--text"></v-btn>
   </div>
 </template>
 
