16d9a7b747fe684a8d0051f21de635e5d6581d71
Braden <bmw02002turbo@gmail.com>
2021-01-26 11:34:19 -0800

    Add grid to dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,13 @@
 <template>
-  <div class="home">
-    <h1>Homepage</h1>
-    <v-btn class="pink white--text">Click me</v-btn>
-    <v-btn depressed color="pink">Click me</v-btn>
-    <v-btn flat color="pink">Click me</v-btn>
-    <v-btn depressed class="pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>Email me</span>
-    </v-btn>
-    <v-btn small depressed class="pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>Email me</span>
-    </v-btn>
-    <v-btn large depressed class="pink white--text">
-      <span>Email me</span>
-      <v-icon right>mdi-email</v-icon>
-    </v-btn>
-    <v-btn fab depressed class="pink white--text">
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
-    <v-btn class="hidden-md-and-down">Hidden for middle and down</v-btn>
-    <v-btn class="hidden-md-and-up">Hidden for middle and up</v-btn>
+  <div class="dashboard">
+    <h1 class="subtitle-1 grey--text">Dashboard</h1>
+    <v-container class="my-5">
+      <v-row>
+        <v-col cols="12" md="6" m-6
+          ><v-btn outline block class="primary">1</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
