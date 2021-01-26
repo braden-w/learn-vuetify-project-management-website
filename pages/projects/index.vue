80e88fc05dd1047d62ebd72ed614994d3a2efb3e
Braden <bmw02002turbo@gmail.com>
2021-01-26 11:34:30 -0800

    Projects and teams page

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="projects"><h1>Projects</h1></div>
+  <div class="projects">
+    <h1 class="subtitle-1 grey--text">Projects</h1>
+    <v-container class="my-5">Content</v-container>
+  </div>
 </template>
 
 <script>
--- pages/team/index.vue
+++ pages/team/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="team"><h1>Team</h1></div>
+  <div class="team">
+    <h1 class="subtitle-1 grey--text">Team</h1>
+    <v-container class="my-5">Content</v-container>
+  </div>
 </template>
 
 <script>
