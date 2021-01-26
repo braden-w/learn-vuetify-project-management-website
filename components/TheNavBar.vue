a24e815b495503d50e151427d6d543e6b019329b
Braden <bmw02002turbo@gmail.com>
2021-01-26 15:24:37 -0800

    Modified app bar instead of toolbar (bug fix)

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,17 +1,17 @@
 <template>
   <nav>
-    <v-toolbar flat app>
+    <v-app-bar app>
       <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer">
       </v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span><span>App</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn flat color="grey">
+      <v-btn color="grey">
         <span>Sign Out</span>
         <v-icon right>mdi-exit-to-app</v-icon>
       </v-btn>
-    </v-toolbar>
+    </v-app-bar>
     <v-navigation-drawer v-model="drawer" app class="primary">
       <div class="d-flex flex-column align-center justify-center">
         <v-avatar size="100">
