260c9f960a4f405eb300f8889d36f42bd85c2d29
Braden <bmw02002turbo@gmail.com>
2021-01-26 09:46:02 -0800

    NavBar Reactivity

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,9 +1,31 @@
 <template>
-  <nav><v-toolbar flat>Yep</v-toolbar></nav>
+  <nav>
+    <v-toolbar flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer">
+      </v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span><span>App</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat color="grey">
+        <span>Sign Out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-navigation-drawer v-model="drawer" app class="primary">
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      drawer: false,
+    }
+  },
+}
 </script>
 
 <style></style>
