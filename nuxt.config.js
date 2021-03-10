c7356cbc3737b1e068d3177ffe286d957d9d964c
Braden <bmw02002turbo@gmail.com>
2021-03-10 10:53:12 -0800

    Updated title

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - learn-vuetify-project-management-website',
-    title: 'learn-vuetify-project-management-website',
+    titleTemplate: '%s - TodoApp',
+    title: 'TodoApp',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
